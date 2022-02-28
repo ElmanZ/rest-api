13028bdd7bad5604bc96ae9eea34d4853ed1c5fd
Elman Zakirov <elmanzakirov@gmail.com>
2022-02-28 17:40:26 +0300

    implemented main api logic

--- db/service.go
+++ db/service.go
@@ -7,12 +7,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gorilla/mux" // this package is used to handle url routes
-	_ "github.com/lib/pq"    // pstgres driver
+	_ "github.com/lib/pq"    // postgres driver
 )
 
-//Service struture allows us to expose router and database
+//Service struture allows us to expose a router and a database
 type Service struct {
 	Router *mux.Router
 	DB     *sql.DB
@@ -33,7 +34,11 @@ func (s *Service) Init(host, port, user, password, database, sslmode string) {
 
 //Defining routes that will use handlers
 func (s *Service) routes() {
-	s.Router.HandleFunc("/users/add", s.addUser).Methods("POST")
+	s.Router.HandleFunc("/user/add", s.addUser).Methods("POST")
+	s.Router.HandleFunc("/chat/add", s.addChat).Methods("POST")
+	s.Router.HandleFunc("/user/update/{id}", s.UpdateUser).Methods("PUT")
+	s.Router.HandleFunc("/chat/get/{id}", s.getChat).Methods("GET")
+	s.Router.HandleFunc("/chat/delete/{id}", s.deleteChat).Methods("DELETE")
 }
 
 //This method start the application
@@ -41,19 +46,105 @@ func (s *Service) Start(addr string) {
 	log.Fatal(http.ListenAndServe(os.Getenv("HTTP_PORT"), s.Router))
 }
 
-//This method is a handler to add a user to the database and return the id
-func (s *Service) addUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Contet-type", "application/json")
+//Method that will procces erros
+func errResp(rw http.ResponseWriter, code int, message string) {
+	jsonResp(rw, code, message)
+}
+
+//Method will process good responses
+func jsonResp(rw http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)                               //marshaling the struct
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8") //setting up response headers for http req
+	rw.WriteHeader(code)                                               //http status code
+	rw.Write(response)                                                 //json response writer
+}
+
+//A handler to add a user to the database and return the id
+func (s *Service) addUser(rw http.ResponseWriter, r *http.Request) {
 	var u User
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&u); err != nil {
-		log.Fatal(err, "Invalid request")
+		errResp(rw, http.StatusBadRequest, "Invalid request")
 	}
+	defer r.Body.Close()
 
 	if err := u.addUser(s.DB); err != nil {
-		return
+		errResp(rw, http.StatusInternalServerError, err.Error())
 	}
-	fmt.Fprintf(w, "User with id %d, created successfully\n", u.User_ID)
+	jsonResp(rw, http.StatusCreated, u)
 }
 
-//Add other crud operations
+//A handler to create a chat between users and return chat id
+func (s *Service) addChat(rw http.ResponseWriter, r *http.Request) {
+	var c Chat
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&c); err != nil {
+		errResp(rw, http.StatusBadRequest, "Invalid request")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := c.addChat(s.DB); err != nil {
+		errResp(rw, http.StatusInternalServerError, err.Error())
+		return
+	}
+	jsonResp(rw, http.StatusCreated, c)
+}
+
+//Updates existing user
+func (s *Service) UpdateUser(rw http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		errResp(rw, http.StatusBadRequest, "Invalid User ID")
+		log.Fatal(err.Error())
+	}
+
+	var u User
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&u); err != nil {
+		errResp(rw, http.StatusBadRequest, "Invalid request")
+	}
+	defer r.Body.Close()
+
+	u.User_ID = id
+	if err := u.updateUser(s.DB); err != nil {
+		errResp(rw, http.StatusInternalServerError, err.Error())
+		return
+	}
+	jsonResp(rw, http.StatusCreated, u)
+}
+
+//A handler to get all chats of a user
+func (s *Service) getChat(rw http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		errResp(rw, http.StatusBadRequest, "Invalid Chat ID")
+		log.Fatal(err.Error())
+	}
+
+	c := Chat{Chat_ID: (id)}
+	if err := c.getChat(s.DB); err != nil {
+		errResp(rw, http.StatusInternalServerError, err.Error())
+		return
+	}
+	jsonResp(rw, http.StatusOK, c)
+}
+
+//A handler to delete the chat from database
+func (s *Service) deleteChat(rw http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		errResp(rw, http.StatusBadRequest, "Invalid Chat ID")
+		return
+	}
+
+	c := Chat{Chat_ID: (id)}
+	if err := c.deleteChat(s.DB); err != nil {
+		errResp(rw, http.StatusInternalServerError, err.Error())
+		return
+	}
+	jsonResp(rw, http.StatusOK, "successfully deleted chat!")
+}
